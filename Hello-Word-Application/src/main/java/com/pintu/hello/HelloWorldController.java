0b6c9983fea1f776164e5cc7a8f7317ecb82fa6a
pintu <pintu@192.168.1.103>
2020-05-13 11:50:30 +0530

    First Commit master

---
+++ Hello-Word-Application/src/main/java/com/pintu/hello/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.pintu.hello;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name) {
+		return "Hello! " + name;
+	}
+}
